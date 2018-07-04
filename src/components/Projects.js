501b3d1cc3706f3f927b372fa8f4491ad2b09ef5
jbryyan <jbryyan@gmail.com>
2018-07-03 23:57:49 -0700

    Fullstack project css complete.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,32 +12,61 @@ class Projects extends Component {
   render() {
     return (
       <div className='projects-container'>
-        <Navbar />
+        <div className='projects-navbar'>
+          <h1 className='projects-header'>Bryan Juarez Projects</h1>
+          <div className='projects-links'>
+            <a href='fullstack'>Fullstack Projects</a>
+            <a href='frontend'>Frontend Projects</a>
+            <a href='data-visualization'>Data Visualization Projects</a>
+          </div>
+        </div>
         <div className='list-container'>
           <div className='list-item'>
-            <div className='list-images'>
+            <div className='list-images '>
               <a href={data[0].link}>
                 <img className='list-image-home' src={image2} />
                 <img className='list-image-main' src={image1} />
               </a>
             </div>
             <div className='list-description'>
-              <h2>Nightlife App</h2>
-              <p>A full stack project that allows users to search for nightlife venues in a specific city and save up their plans.</p>
-              <p>Techstack: React + Redux + SemanticUI React + Node.js + Express + MongoDB</p>
-              <p>Website | Repo </p>
+              <div className='description-text'>
+                <h2>Nightlife App</h2>
+                <p>A full stack project that allows users to search for nightlife venues in a specific city and save up their plans.</p>
+                <p>Techstack: React + Redux + SemanticUI React + Node.js + Express + MongoDB</p>
+                <p>Website | Repo </p>
+              </div>
             </div>
           </div>
           <div className='list-item'>
-            <a href={data[0].link}>
-              <img className='list-image-home' src={image2} />
-              <img className='list-image-main' src={image1} />
-            </a>
+            <div className='list-images '>
+              <a href={data[0].link}>
+                <img className='list-image-home' src={image2} />
+                <img className='list-image-main' src={image1} />
+              </a>
+            </div>
             <div className='list-description'>
-              <h2>Nightlife App</h2>
-              <p>A full stack project that allows users to search for nightlife venues in a specific city and save up their plans.</p>
-              <p>Techstack: React + Redux + SemanticUI React + Node.js + Express + MongoDB</p>
-              <p>Website | Repo </p>
+              <div className='description-text'>
+                <h2>Nightlife App</h2>
+                <p>A full stack project that allows users to search for nightlife venues in a specific city and save up their plans.</p>
+                <p>Techstack: React + Redux + SemanticUI React + Node.js + Express + MongoDB</p>
+                <p>Website | Repo </p>
+              </div>
+            </div>
+          </div>
+          <div className='list-item'>
+            <div className='list-images '>
+              <a href={data[0].link}>
+                <img className='list-image-home' src={image2} />
+                <img className='list-image-main' src={image1} />
+              </a>
+            </div>
+            <div className='list-description'>
+              <div className='description-text'>
+                <h2>Nightlife App</h2>
+                <p>A full stack project that allows users to search for nightlife venues in a specific city and save up their plans.</p>
+                <p>Techstack: React + Redux + SemanticUI React + Node.js + Express + MongoDB</p>
+                <p>Website | Repo </p>
+              </div>
             </div>
           </div>
         </div>
